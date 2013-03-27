b107e467cc0271529696ac388d612b4420915adf
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2013-03-27 17:58:24 -0500

    Add a couple more basic tests

--- inst/tests/test_brew.R
+++ inst/tests/test_brew.R
@@ -1,3 +1,42 @@
+test_that("code runs without output", {
+  env <- new.env()
+  output <- capture.output(brew(text="<% x <- 123 %>", envir=env))
+  expect_equal(output, "")
+  expect_equal(env$x, 123)
+})
+
+test_that("regular text at the beginning of a line with code", {
+  env <- new.env()
+  output <- capture.output(brew(text="foo <% x <- 123 %>", envir=env))
+  expect_equal(output, "foo ")
+})
+
+test_that("regular text at the end of a line with code", {
+  env <- new.env()
+  output <- capture.output(brew(text="<% x <- 123 %> foo", envir=env))
+  expect_equal(output, " foo")
+})
+
+test_that("comments are swallowed", {
+  output <- capture.output(brew(text="<%# foo %>"))
+  expect_equal(output, "")
+})
+
+test_that("code runs with output", {
+  output <- capture.output(brew(text="<%= 123 %>"))
+  expect_equal(output, "123")
+})
+
+test_that("templates are output correctly", {
+  output <- capture.output(brew(text="<%%= 123 %%>"))
+  expect_equal(output, "<%= 123 %>")
+})
+
+test_that("templates with extra percents are reduced correctly", {
+  output <- capture.output(brew(text="<%%%= 123 %%%>"))
+  expect_equal(output, "<%%= 123 %%>")
+})
+
 test_that("incomplete expressions are executed properly", {
   expected <- c("Incomplete expression tests", "1 2 3 4 5 OK")
   actual <- capture.output(brew('../incomplete.brew'))
